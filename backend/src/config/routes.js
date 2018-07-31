d472083ca519d12dc24b0bef8629ed54c8e75e3c
Vinicius Barbosa <viniciusnep1@hotmail.com>
2018-07-31 19:36:16 -0300

    enable cors

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 module.exports = function(server) {
     //API Routes
     const router = express.Router()
-    server.use('api/', router)
+    server.use('/api', router)
 
     //TODO Routes
     const todoService = require('../api/todo/todoService')
